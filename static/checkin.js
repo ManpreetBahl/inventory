9dee2cf4d9a352255e4e24571201614ebb87654c
Manpreet Bahl <manpreet@pdx.edu>
2017-12-05 17:08:26 -0800

    Checkin.js styled

--- static/checkin.js
+++ static/checkin.js
@@ -1,26 +1,48 @@
+/*#################################################################################
+JavaScript file for Checkin HTML Page
+Manpreet Bahl
+Inventory Management System
+#################################################################################*/
+
+/*#################################################################################
+The function below toggles between displaying the checkin cart table or hiding it.
+If there are no items in the checkin table, it hides the table. When an item is
+added and the table is hidden, this function unhides it. The function also toggles
+between disabling and renabling buttons based on if the check in table is being
+displayed and hidden.
+#################################################################################*/
 function check() {
+    //Hide checkin table and disable buttons if there's nothing in checkin table
     if( (jQuery("#checkinTable > tbody").children("tr").length) <= 0) {
         jQuery("#checkin").hide();
         jQuery("#checkinButton").prop('disabled', true);
         jQuery("#remove").prop('disabled', true);
     }
     else{
+        //Show the checkin table and enable button
         jQuery("#checkin").show();
         jQuery("#checkinButton").prop('disabled', false);
         jQuery("#remove").prop('disabled', false);
     }
 }
+//#################################################################################
 
-//Code was obtained from https://stackoverflow.com/questions/3458571/jquery-click-event-on-tr-elements-with-in-a-table-and-getting-td-element-v
+/*#################################################################################
+# This function contains the code for the various buttons present on the HTML page
+#################################################################################*/
+//Code for clikc event was obtained from https://stackoverflow.com/questions/3458571/jquery-click-event-on-tr-elements-with-in-a-table-and-getting-td-element-v
 $(document).ready(function(){
     jQuery("#checkin").hide();
 
+    //When a table row is clicked in the inventory table, it is added to the checkin table.
+    //If the item already exists, the checkin quantity is incremented by 1
     $("#inventoryTable > tbody > tr").on("click", ".clickable-row", function(event){        
         var id = $(this).attr("id"); //This line was found at https://stackoverflow.com/questions/1618209/jquery-how-to-get-the-value-of-id-attribute
         var itemName = $(this).find("#name > .tablesaw-cell-content").text().trim();
         var desc = $(this).find("#desc > .tablesaw-cell-content").text().trim();
         var quantity = $(this).find("#quantity > .tablesaw-cell-content").text().trim();
 
+        //If the item exists in the checkin table, increment the quantity by 1
         var inTable = false;
         jQuery("#checkinTable tbody").find("tr").each(function(){
             var itemID = jQuery(this).attr("id");
@@ -31,12 +53,15 @@ $(document).ready(function(){
             }
         });
 
+        //If the item does not exist, add the item to the table
         if(inTable === false){
             jQuery('#checkinTable tbody').append("<tr id=\"" + id + "\"><td><input type=\"checkbox\" id=\"checkbox\"></input></td><td>" + itemName + "</td><td>" + desc + "</td><td><input type=\"number\" id=\"quantityWanted\" value=\"1\" min=\"1\" max=\"" + quantity + "\"/></td></tr>"); 
         }
-        check();
+        check(); //Check to see if the checkin table should be still hidden or displayed
     });
 
+    //When this button is clicked, it packages up the checkin table and sends a POST
+    //request to the server for processing
     $("#checkinButton").on("click", function(event){
         //Get table data and convert it into JSON
         //Code below was taken from http://johndyer.name/html-table-to-json/ and modified to meet the needs
@@ -63,14 +88,15 @@ $(document).ready(function(){
             cart[k]["checkinquantity"] = quantityWanted[k].value;
         }
 
+        //Send AJAX POST request
         jQuery.ajax({
             type: "POST",
             url: "/checkin",
             contentType: "application/json",
             data: JSON.stringify(cart),
             success: function(){
-                alert("Successfully checked in items!");
-                location.reload(true);
+                alert("Successfully checked in items!"); //Notify user that the transactions were successful
+                location.reload(true); //Reload the page to show the updated inventory table
             },
             error: function(err){
                 alert("Unable to checkin!\n" + "Status: " + err.status + "\nError Message: " + err.statusText);
@@ -78,6 +104,9 @@ $(document).ready(function(){
         });        
     });
 
+    //When this button is clicked, it will remove all items that are checked in the 
+    //check in table. If removing an item results in a table with no rows, the table
+    //is hidden due to the call to check function.
     $("#remove").on("click", function(event){
         $("#checkinTable tbody").find("tr").each(function(){
             if( $(this).find('#checkbox').is(":checked")){
@@ -87,3 +116,4 @@ $(document).ready(function(){
         check();
     });
 });
+//#################################################################################
